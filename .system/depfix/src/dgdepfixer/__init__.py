ad02feae8142bc01a04c0831ec9ac3fd8f4c952d
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2023-05-12 22:37:31 +0200

    update dgdepfixer sw versions

--- .system/depfix/src/dgdepfixer/__init__.py
+++ .system/depfix/src/dgdepfixer/__init__.py
@@ -6,16 +6,16 @@ __email__ = 'ue.sse@nnamlettik.samoht'[::-1]
 
 swversions={
     #recommended versions for all deps are typically set to the most recent production release at the time of testing.
-    'CMake': dict(oldest='3.15.3',recommended='3.24.2',newest=None),#v3.8.2 supports C++17, v3.12.4 supports FindPython3 and C++20. v3.12.3 comes with Conda, we hope it is good enough. v3.15 has improved search policies for python.
+    'CMake': dict(oldest='3.15.3',recommended='3.26.3',newest=None),#v3.8.2 supports C++17, v3.12.4 supports FindPython3 and C++20. v3.12.3 comes with Conda, we hope it is good enough. v3.15 has improved search policies for python.
     'git':dict(oldest='1.8.3.1'),#v1.8.3.1 is available out of the box on lxplus and DMSC.
     'pymod:numpy':dict(oldest='1.17.0'),#1.17.0 is first release without python2 support.
-    'pymod:matplotlib':dict(oldest='3.1.3'),#3.0.0 is first release without python2 support. NB: DMSC with 3.5 only supports up to 3.0.3 (THIS COMMENT IS OUTDATED DMSC NOW HAVE 3.6). NB: Matplotlib 3.2.1 introduced dependency on cm-super font, which dgdepfixer would need to look for!!! See also DGSW-520 before updating recommended version.
-    'pymod:ptpython':dict(oldest='2.0.4',recommended='3.0.20'),#NB: Keep a recommended version for ptpython now  (see special ptpython treatment in platformutils.py)
+    'pymod:matplotlib':dict(oldest='3.3.4'),#3.0.0 is first release without python2 support. NB: DMSC with 3.5 only supports up to 3.0.3 (THIS COMMENT IS OUTDATED DMSC NOW HAVE 3.6). NB: Matplotlib 3.2.1 introduced dependency on cm-super font, which dgdepfixer would need to look for!!! See also DGSW-520 before updating recommended version.
+    'pymod:ptpython':dict(oldest='2.0.4',recommended='3.0.23'),#NB: Keep a recommended version for ptpython now  (see special ptpython treatment in platformutils.py)
     #TODO: Other interesting ones: 'h5py','pyqt5', ipython,... (note: 'PyNE' is NOT the nuclear one)
 }
 
 #Pinned because of https://github.com/mctools/dgcode/issues/1 :
-swversions['pymod:matplotlib']['recommended']='3.3.4'
+#swversions['pymod:matplotlib']['recommended']='3.3.4'
 
 
 #import sys
