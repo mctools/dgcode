57135071f331d0ae0bc2eda583916b875d78cdff
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2023-10-16 12:21:25 +0200

    dgdepfixer installs pypi-pybind11

--- .system/depfix/src/dgdepfixer/__init__.py
+++ .system/depfix/src/dgdepfixer/__init__.py
@@ -7,6 +7,7 @@ __email__ = 'ue.sse@nnamlettik.samoht'[::-1]
 swversions={
     #recommended versions for all deps are typically set to the most recent production release at the time of testing.
     'CMake': dict(oldest='3.24.2',recommended='3.26.4',newest=None),
+    'pybind11':dict(oldest='2.10.4'),
     'git':dict(oldest='1.8.3.1'),#v1.8.3.1 is available out of the box on lxplus and DMSC.
     'pymod:numpy':dict(oldest='1.17.0'),#1.17.0 is first release without python2 support.
     'pymod:matplotlib':dict(oldest='3.3.4'),#3.0.0 is first release without python2 support. NB: DMSC with 3.5 only supports up to 3.0.3 (THIS COMMENT IS OUTDATED DMSC NOW HAVE 3.6). NB: Matplotlib 3.2.1 introduced dependency on cm-super font, which dgdepfixer would need to look for!!! See also DGSW-520 before updating recommended version.
