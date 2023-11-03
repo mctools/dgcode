384d35f83667a463b0f29d3b90fcdc2e908850e5
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2023-11-03 10:59:43 +0100

    cleanup

--- packages/Framework/Core/libinc/Python.hh
+++ packages/Framework/Core/libinc/Python.hh
@@ -25,36 +25,35 @@
 #endif
 
 #include <pybind11/pybind11.h>
+
+//Convenience alias of pybind11 namespace:
 namespace py = pybind11;
 
+//A few extra convenience functions in the pyextra namespace:
 namespace pyextra {
 
   //If using python modules from inside regular (libsrc/* or app_*/*) non-python
-  //C++ code, one must ensure python has been initialised (for app_*/* code, one
-  //is most likely better off using a pybind11 scoped_interpreter).
+  //C++ code, one must ensure python has been initialised (although for app_*/*
+  //code, one is most likely better off using a pybind11 scoped_interpreter).
   inline bool isPyInit() { return Py_IsInitialized(); }
-
   void pyInit( int argc, char** argv );//Transfer C++ cmdline to sys.argv
   void pyInit( const char * argv0 = nullptr );//only provide sys.argv[0] (defaulting to "dummyargv0")
-
   inline void ensurePyInit()
   {
     if (!Py_IsInitialized())
       pyInit();
   }
-
+  //Importing modules (same as py::module_::import):
   inline py::object pyimport( const char * name ) { return py::module_::import(name); }
 }
 
-//FIXME: Obsolete macro:
-#define PYTHON_MODULE3( mod ) PYBIND11_MODULE(PYMODNAME, mod)
-
+//Convenience macro for defining a python module:
 #ifdef PYTHON_MODULE
 #  undefine PYTHON_MODULE
 #endif
 #define PYTHON_MODULE( modvarname ) PYBIND11_MODULE(PYMODNAME, modvarname)
 
-//sbld_stringify macro here for convenience:
+//Convenience macros for stringification:
 #ifdef sbld_xstringify
 #  undefine sbld_xstringify
 #endif
